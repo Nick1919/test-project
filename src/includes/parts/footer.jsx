c08cd7f14b253580a0572d44ed70f59750a48571
Nick Flores <nick.flores@whiz.pe>
2022-11-20 02:01:10 -0500

    footer

--- src/includes/parts/footer.jsx
+++ src/includes/parts/footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
 			<section id="footer">
 				<Container>
 					<Row>
-						<Col xs={12} md={6}>
+						<Col xs={12}>
 							<h1>Footer</h1>
 						</Col>
 					</Row>
